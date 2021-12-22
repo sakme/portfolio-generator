86d2cc9ebfc1cdaeab8344fc3a9913733955bdf9
sakme <alan@akme.us>
2021-12-22 15:28:30 -0500

    added about confirmation

--- app.js
+++ app.js
@@ -31,9 +31,22 @@ const promptUser = () => {inquirer
             message: 'Enter your GitHub Username.'
         },
         {
+            type: 'confirm',
+            name: 'confirmAbout',
+            message: 'Would you like to enter some information about yourself for and "About" section?',
+            default: true
+        },
+        {
             type: 'input',
             name: 'about',
-            message: 'Provide some information about yourself.'
+            message: 'Provide some information about yourself.',
+            when: ({ confirmAbout }) => {
+                if (confirmAbout) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
         }
     ]);
 };
